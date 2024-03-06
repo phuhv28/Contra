ab088077626e1bd472c7143f0f65b96c8ca614e1
HoangPhu <phuhv102@gmail.com>
2024-03-06 20:08:37 +0700

    Add init function

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,54 @@
 #include "Constant.h"
 
+
+
+bool init(SDL_Renderer *renderer, SDL_Window *window)
+{
+	bool success = true;
+
+	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+	{
+		std::cout << "SDL could not initialize! SDL Error:\n" << SDL_GetError();
+		success = false;
+	}
+	else
+	{
+		if( !SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ) )
+		{
+			std::cout << "Warning: Linear texture filtering not enabled!";
+		}
+
+		window = SDL_CreateWindow( "Contra", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+		if( window == NULL )
+		{
+			std::cout << "Window could not be created! SDL Error: \n" << SDL_GetError();
+			success = false;
+		}
+		else
+		{
+			renderer = SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC );
+			if( renderer == NULL )
+			{
+				std::cout << "Renderer could not be created! SDL Error:\n" << SDL_GetError();
+				success = false;
+			}
+			else
+			{
+				SDL_SetRenderDrawColor( renderer, 0xFF, 0xFF, 0xFF, 0xFF );
+
+				int imgFlags = IMG_INIT_PNG;
+				if( !( IMG_Init( imgFlags ) & imgFlags ) )
+				{
+					std::cout << "SDL_image could not initialize! SDL_image Error:\n" << IMG_GetError();
+					success = false;
+				}
+			}
+		}
+	}
+
+	return success;
+}
+
 int main(int argc, char *argv[])
 {
     std::cout << "Hello World!";
