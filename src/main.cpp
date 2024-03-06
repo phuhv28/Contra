27a70b84eeffe2dceb25118d6627bcabaef97ba3
HoangPhu <phuhv102@gmail.com>
2024-03-06 20:27:21 +0700

    Add close() in main.cpp and modify main

--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,108 @@
 #include "Constant.h"
+#include "Object.h"
 
+SDL_Window *window;
+SDL_Renderer *renderer;
+Object backGround;
 
-
-bool init(SDL_Renderer *renderer, SDL_Window *window)
+bool init()
 {
-	bool success = true;
+    bool success = true;
 
-	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-	{
-		std::cout << "SDL could not initialize! SDL Error:\n" << SDL_GetError();
-		success = false;
-	}
-	else
-	{
-		if( !SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ) )
-		{
-			std::cout << "Warning: Linear texture filtering not enabled!";
-		}
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        std::cout << "SDL could not initialize! SDL Error:\n"
+                  << SDL_GetError();
+        success = false;
+    }
+    else
+    {
+        if (!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1"))
+        {
+            std::cout << "Warning: Linear texture filtering not enabled!";
+        }
 
-		window = SDL_CreateWindow( "Contra", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-		if( window == NULL )
-		{
-			std::cout << "Window could not be created! SDL Error: \n" << SDL_GetError();
-			success = false;
-		}
-		else
-		{
-			renderer = SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC );
-			if( renderer == NULL )
-			{
-				std::cout << "Renderer could not be created! SDL Error:\n" << SDL_GetError();
-				success = false;
-			}
-			else
-			{
-				SDL_SetRenderDrawColor( renderer, 0xFF, 0xFF, 0xFF, 0xFF );
+        window = SDL_CreateWindow("Contra", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        if (window == NULL)
+        {
+            std::cout << "Window could not be created! SDL Error: \n"
+                      << SDL_GetError();
+            success = false;
+        }
+        else
+        {
+            renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+            if (renderer == NULL)
+            {
+                std::cout << "Renderer could not be created! SDL Error:\n"
+                          << SDL_GetError();
+                success = false;
+            }
+            else
+            {
+                SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
 
-				int imgFlags = IMG_INIT_PNG;
-				if( !( IMG_Init( imgFlags ) & imgFlags ) )
-				{
-					std::cout << "SDL_image could not initialize! SDL_image Error:\n" << IMG_GetError();
-					success = false;
-				}
-			}
-		}
-	}
+                int imgFlags = IMG_INIT_PNG;
+                if (!(IMG_Init(imgFlags) & imgFlags))
+                {
+                    std::cout << "SDL_image could not initialize! SDL_image Error:\n"
+                              << IMG_GetError();
+                    success = false;
+                }
+            }
+        }
+    }
 
-	return success;
+    return success;
 }
 
+void close()
+{
+    backGround.free();
+    SDL_DestroyWindow(window);
+    window = NULL;
+    SDL_DestroyRenderer(renderer);
+    renderer = NULL;
+}
 int main(int argc, char *argv[])
 {
-    std::cout << "Hello World!";
+
+    // Start up SDL and create window
+    if (!init)
+    {
+        std::cout << "Failed to initialize!\n";
+    }
+    else
+    {
+        // Load media
+        if (!backGround.LoadIMG("res/background.png", renderer))
+        {
+            std::cout << "Failed to load media!\n";
+        }
+        else
+        {
+            bool quit = false;
+
+            SDL_Event e;
+
+            while (!quit)
+            {
+                while (SDL_PollEvent(&e) != 0)
+                {
+                    if (e.type == SDL_QUIT)
+                    {
+                        quit = true;
+                    }
+                }
+                SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+                SDL_RenderClear(renderer);
+
+                backGround.render(renderer);
+            }
+        }
+    }
+
+    close();
 
     return 0;
 }
